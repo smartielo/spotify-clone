c26b18f068b2b532e5c5d83360886451ed5097b8
Gabriel Martielo <gabrielmartielo3214@gmail.com>
2025-04-16 15:43:30 -0300

    Primeiro Commit

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/style.css'; // Assuming you have a CSS file for styling
+
+
+const dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to the dashboard!</p>
+    </div>
+  );
+}
+
+export default dashboard;
